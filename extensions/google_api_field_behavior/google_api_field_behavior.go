996ed535e250b3ab216436fdd2602c3745a3b959
bachurin.pavel <bachurin.pavel@wb.ru>
2025-06-28 00:05:13 +0300

    add field behavior parsing

---
+++ extensions/google_api_field_behavior/google_api_field_behavior.go
@@ -0,0 +1,39 @@
+package extensions
+
+import (
+	"github.com/pseudomuto/protoc-gen-doc/extensions"
+	"google.golang.org/genproto/googleapis/api/annotations"
+)
+
+// FieldBehaviorExtension contains the field behavior values set by the (google.api.field_behavior) field option extension.
+type FieldBehaviorExtension struct {
+	Behaviors []string `json:"behaviors"`
+}
+
+func init() {
+	extensions.SetTransformer("google.api.field_behavior", func(payload interface{}) interface{} {
+		// Try different possible types
+		switch v := payload.(type) {
+		case []annotations.FieldBehavior:
+			var behaviors []string
+			for _, behavior := range v {
+				behaviors = append(behaviors, behavior.String())
+			}
+			return FieldBehaviorExtension{Behaviors: behaviors}
+		case annotations.FieldBehavior:
+			return FieldBehaviorExtension{Behaviors: []string{v.String()}}
+		case []interface{}:
+			var behaviors []string
+			for _, item := range v {
+				if behavior, ok := item.(annotations.FieldBehavior); ok {
+					behaviors = append(behaviors, behavior.String())
+				}
+			}
+			if len(behaviors) > 0 {
+				return FieldBehaviorExtension{Behaviors: behaviors}
+			}
+		}
+
+		return nil
+	})
+}
---
+++ extensions/google_api_field_behavior/google_api_field_behavior_test.go
@@ -0,0 +1,40 @@
+package extensions_test
+
+import (
+	"testing"
+
+	"github.com/pseudomuto/protoc-gen-doc/extensions"
+	. "github.com/pseudomuto/protoc-gen-doc/extensions/google_api_field_behavior"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/genproto/googleapis/api/annotations"
+)
+
+func TestTransform(t *testing.T) {
+	// Test with single behavior
+	behavior := annotations.FieldBehavior_REQUIRED
+	transformed := extensions.Transform(map[string]interface{}{"google.api.field_behavior": behavior})
+	require.NotEmpty(t, transformed)
+
+	fieldBehavior := transformed["google.api.field_behavior"].(FieldBehaviorExtension)
+	assert.Equal(t, []string{"REQUIRED"}, fieldBehavior.Behaviors)
+
+	// Test with multiple behaviors
+	behaviors := []annotations.FieldBehavior{
+		annotations.FieldBehavior_REQUIRED,
+		annotations.FieldBehavior_OUTPUT_ONLY,
+	}
+	transformed = extensions.Transform(map[string]interface{}{"google.api.field_behavior": behaviors})
+	require.NotEmpty(t, transformed)
+
+	fieldBehavior = transformed["google.api.field_behavior"].(FieldBehaviorExtension)
+	assert.Equal(t, []string{"REQUIRED", "OUTPUT_ONLY"}, fieldBehavior.Behaviors)
+
+	// Test with nil payload
+	transformed = extensions.Transform(map[string]interface{}{"google.api.field_behavior": nil})
+	assert.Empty(t, transformed)
+
+	// Test with wrong type
+	transformed = extensions.Transform(map[string]interface{}{"google.api.field_behavior": "not a behavior"})
+	assert.Empty(t, transformed)
+}
